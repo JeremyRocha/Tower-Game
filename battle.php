d08a736514707b0b9db9a10b08ef8f46662e96fd
Kelougin <200618875@student.georgianc.on.ca>
2025-03-29 19:33:14 -0400

    March 27th

--- battle.php
+++ battle.php
@@ -1,1 +1,13 @@
 <?php
+
+class Player{
+    private $playerHealth = 100;
+    private $playerWallet = 100;
+}
+abstract class Enemy{
+    private $enemyHealth;
+}
+
+class Game {
+
+}
--- index.php
+++ index.php
@@ -1,3 +1,2 @@
 <?php
 require 'database.php';
-echo"Database connected!";
