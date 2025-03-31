be0670f7f71037895a685ba04475a8d26652a903
Kelougin <200618875@student.georgianc.on.ca>
2025-03-31 16:49:37 -0400

    March 31st

--- database.php
+++ database.php
@@ -12,4 +12,3 @@ try{
 }catch (PDOException $e){
     die("Connection failed: " . $e->getMessage()); //Display error
 }
-#session_start(): Ignoring session_start() because a session is already active in C:\Users\user\Desktop\Georgian Year - 1\Semester 2\PhP\TowerGame\login.php on line 2
--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 include 'database.php'; //Includes logic for database
 
 class Login{
