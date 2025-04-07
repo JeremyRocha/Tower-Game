ba175151dc991456981ddc0c9d292e9de3233c34
Kelougin <200618875@student.georgianc.on.ca>
2025-04-06 21:08:38 -0400

    March 31st

--- shop.php
+++ shop.php
@@ -130,6 +130,7 @@ if(isset($_SESSION['username'])){
     $shop = new Shop($player);
     if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['items'])){
         $shop->purchase();
+        $_SESSION['player'] = serialize($player);
     }else{
         $shop->showShop();
     }
